186c9d8603e750ff1a1ad36027f1836653c14c67
Adriana Cividini <121236952+AdrianaCividini@users.noreply.github.com>
2024-05-09 15:39:10 +0200

    Created the todo LIst in Vite.

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import { useState } from "react";
 
-export default function TodoInput() {
+export default function TodoInput(props) {
+  const { handleAddTodos, todoValue, setTodoValue } = props;
   return (
     <header>
-      <input type="text" placeholder="Enter" />
-      <button>Enter</button>
+      <input
+        value={todoValue}
+        onChange={(e) => {
+          setTodoValue(e.target.value);
+        }}
+        placeholder="Enter todo..."
+      />
+      <button
+        onClick={() => {
+          handleAddTodos(todoValue);
+          setTodoValue("");
+        }}
+      >
+        Add
+      </button>
     </header>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import TodoCard from "./TodoCard";
 
-export default function TodoList() {
-  let todos = ["Go to the gym", "Eat more fruits and vege", "Pick up the dog"];
+export default function TodoList(props) {
+  const { todos } = props;
 
   return (
     <ul className="main">
       {todos.map((todo, todoIndex) => {
         return (
-          <TodoCard key={todoIndex}>
+          <TodoCard {...props} key={todoIndex} index={todoIndex}>
             <p>{todo}</p>
           </TodoCard>
         );
